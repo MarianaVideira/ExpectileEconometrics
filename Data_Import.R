5dd57b54456696dafa224a6cb73b3e8ac8296de1
Marlon Francisco <francisco.marlon@gmail.com>
2022-03-06 00:48:08 +0000

    Fixed id issues, sampled 10,000 id to create new database, fixed rowwise sums and created remaining dummies. Ready to start regressions.

--- Data_Import.R
+++ Data_Import.R
@@ -1,17 +1,21 @@
 if(!require("tidyverse")) install.packages("tidyverse")
-library(c("tidyverse", "readr","dplyr"))
+if(!require("easypackages")) install.packages("easypackages") # easier to load multiple packages
+library("tidyverse","readr","dplyr")
 library(haven)
 
+
 # 0.  Data Import  --------------------------------------------------------
+# Free unused memory
+gc()
 
 # Set working directory and file name
 dir <- getwd()
-  list.files(path = dir)
+list.files(path = dir)
 
 
 # *0.1. Using Haven -------------------------------------------------------
 
-filedta <- "marlon_data.dta"
+filedta <- "quadrospessoal.dta"
  
 # Import 
 dat <- read_dta(filedta, encoding = NULL,col_select = NULL, skip = 0,
@@ -19,13 +23,18 @@ dat <- read_dta(filedta, encoding = NULL,col_select = NULL, skip = 0,
                 .name_repair = "unique")
 
 # filter obs year & clear duplicates
-dat <- dat %>% 
-  filter(ano > 2001) %>%
-  distinct()
-
-
-# *0.2. Using CSV ---------------------------------------------------------
-
-# filename <- "qp_data.csv"
+dat <- dat %>% distinct(ano, id,.keep_all=TRUE)
   
-# Data set for wide data and select the observations from 1995-2009
+dat <- dat %>% 
+  filter(ano > 2001)
+
+
+# Use sample of data (10,000 ids)
+nID <- length(unique(dat$id))
+p = 10000/nrow(dat)
+set.seed(5)
+inSampleID <- sample(unique(dat$id), round(nID * p), replace=FALSE)
+data <- dat[dat$id %in% inSampleID, ] 
+
+
+
--- Data_Wrangling.R
+++ Data_Wrangling.R
@@ -3,112 +3,122 @@ if(!require("tidyverse")) install.packages("tidyverse")
 if(!require("data.table")) install.packages("data.table")
 library(tidyverse, data.table)
 
+# Free unused memory
+gc()
+
 # Check Data 
-head(dat)
+head(data)
+
+# Summing Rows
+data <- data %>% 
+  rowwise() %>%
+  mutate(bonus = sum(c(remhextra,outrasprest,subsidios)),
+         wage = sum(c(bonus, rembase)),
+         hours = sum(c(hnormais, hextra)),
+  )
 
 # Renaming Columns and Adding Columns
-dat <- dat %>% 
-  mutate(bonus = sum(remhextra,outrasprest,subsidios),
-         wage = sum(bonus, rembase),
-         lwage = log(wage),
-         lbase = log(rembase),
-         lbonus = log(bonus),
-         hours = sum(hnormais, hextra),
-         salesK = vendas_euro/1000,
-         salespc = salesK/pessoas,
-         gender = case_when(
-           sexo == 1 ~ 1, # male
-           sexo == 2 ~ 0), # female
-         firmS = case_when(
-           pessoas > 9 & pessoas < 20 ~ 1,
-           pessoas > 19 ~ 0,
-           pessoas < 10 ~ 0),
-         firmM = case_when(
-           pessoas > 19 & pessoas < 100 ~ 1,
-           pessoas < 20 ~ 0,
-           pessoas > 99 ~ 0),
-         firmB = case_when(
-           pessoas > 99 & pessoas < 500 ~ 1,
-           pessoas < 100 ~ 0,
-           pessoas > 499 ~ 0),
-         firmL = case_when(
-           pessoas > 499 ~ 1,
-           pessoas < 500 ~ 0),
-         portuguese = case_when(
-           nacion < 101 ~ 1,
-           nacion > 100 ~ 0,
-           is.na(nacion) ~1),
-         primary = case_when(
-           sect_comp > 0 & sect_comp <= 3 ~ 1, 
-           sect_comp > 3 ~ 0),
-         manufL = case_when(
-           sect_comp >= 4 & sect_comp <= 8 ~ 1,
-           sect_comp > 8 ~ 0,
-           sect_comp < 4 ~ 0),
-         manufM = case_when(
-           sect_comp >= 9 & sect_comp <= 14 ~ 1,
-           sect_comp > 14 ~ 0,
-           sect_comp < 9 ~ 0),
-         manufH = case_when(
-           sect_comp >= 15 & sect_comp <= 18 ~ 1,
-           sect_comp > 18 ~ 0,
-           sect_comp < 15 ~ 0),
-         energy = case_when(
-           sect_comp == 19 ~ 1,
-           sect_comp != 19 ~ 0),
-         construction = case_when(
-           sect_comp == 20 ~ 1,
-           sect_comp != 20 ~ 0),
-         services = case_when(
-           sect_comp >= 21 & sect_comp <= 23 ~ 1,
-           sect_comp > 23 ~ 0,
-           sect_comp < 21 ~ 0),
-         finance_realestate = case_when(
-           sect_comp >= 24 & sect_comp <= 25 ~ 1,
-           sect_comp > 25 ~ 0,
-           sect_comp < 24 ~ 0),
-         publicadmin = case_when(
-           sect_comp == 26 ~ 1,
-           sect_comp != 26 ~ 0),
-         education = case_when(
-           sect_comp == 27 ~ 1,
-           sect_comp != 27 ~ 0),
-         health = case_when(
-           sect_comp == 28 ~ 1,
-           sect_comp != 28 ~ 0),
-         others = case_when(
-           sect_comp >= 29 & sect_comp <= 31 ~ 1,
-           sect_comp > 31 ~ 0,
-           sect_comp < 29 ~ 0),
-         primary_others = case_when(
-           sect_comp >= 29 & sect_comp <= 31 ~ 1,
-           sect_comp >= 1 & sect_comp <= 3 ~ 1, 
-           sect_comp > 3 & sect_comp < 29 ~ 0,
-           sect_comp > 31 ~ 0)
-         ) %>%
-  select(-sexo, -vendas_euro, -pessoas, -hnormais, -hextra, -remhextra,-outrasprest,
-         -subsidios, -nacion) %>%
-  rename(year = ano, firm = empresa, wage_base = rembase, college = hab_8910) %>%
+data <- data %>%
+  mutate(
+    lwage = log(wage),
+    lbase = log(rembase),
+    lbonus = log(bonus),
+    salesK = vendas_euro/1000,
+    salespc = salesK/pessoas,
+    gender = case_when(
+      sexo == 1 ~ 1, # male
+      sexo == 2 ~ 0), # female
+    firmS = case_when(
+    pessoas > 9 & pessoas < 20 ~ 1,
+    pessoas > 19 ~ 0,
+    pessoas < 10 ~ 0),
+    firmM = case_when(
+      pessoas > 19 & pessoas < 100 ~ 1,
+      pessoas < 20 ~ 0,
+      pessoas > 99 ~ 0),
+    firmB = case_when(
+      pessoas > 99 & pessoas < 500 ~ 1,
+      pessoas < 100 ~ 0,
+      pessoas > 499 ~ 0),
+    firmL = case_when(
+      pessoas > 499 ~ 1,
+      pessoas < 500 ~ 0),
+    portuguese = case_when(
+      nacion < 101 ~ 1,
+      nacion > 100 ~ 0,
+      is.na(nacion) ~1),
+    primary = case_when(
+      sect_comp > 0 & sect_comp <= 3 ~ 1, 
+      sect_comp > 3 ~ 0),
+    manufL = case_when(
+      sect_comp >= 4 & sect_comp <= 8 ~ 1, 
+      sect_comp > 8 ~ 0,
+      sect_comp < 4 ~ 0),
+    manufM = case_when(
+      sect_comp >= 9 & sect_comp <= 14 ~ 1,
+      sect_comp > 14 ~ 0,
+      sect_comp < 9 ~ 0),
+    manufH = case_when(
+      sect_comp >= 15 & sect_comp <= 18 ~ 1,
+      sect_comp > 18 ~ 0,
+      sect_comp < 15 ~ 0),
+    energy = case_when(
+      sect_comp == 19 ~ 1,
+      sect_comp != 19 ~ 0),
+    construction = case_when(
+      sect_comp == 20 ~ 1,
+      sect_comp != 20 ~ 0),
+    services = case_when(
+      sect_comp >= 21 & sect_comp <= 23 ~ 1,
+      sect_comp > 23 ~ 0,
+      sect_comp < 21 ~ 0),
+    finance_realestate = case_when(
+      sect_comp >= 24 & sect_comp <= 25 ~ 1,
+      sect_comp > 25 ~ 0,
+      sect_comp < 24 ~ 0),
+    publicadmin = case_when(
+      sect_comp == 26 ~ 1,
+      sect_comp != 26 ~ 0),
+    education = case_when(
+      sect_comp == 27 ~ 1,
+      sect_comp != 27 ~ 0),
+    health = case_when(
+      sect_comp == 28 ~ 1,
+      sect_comp != 28 ~ 0),
+    others = case_when(
+      sect_comp >= 29 & sect_comp <= 31 ~ 1,
+      sect_comp > 31 ~ 0,
+      sect_comp < 29 ~ 0),
+    primary_others = case_when(
+      sect_comp >= 29 & sect_comp <= 31 ~ 1,
+      sect_comp >= 1 & sect_comp <= 3 ~ 1, 
+      sect_comp > 3 & sect_comp < 29 ~ 0,
+      sect_comp > 31 ~ 0),
+    highschool = case_when(
+      hab_5 == 1 | hab_67 == 1 | hab_8910 == 1 ~ 1,
+      hab_5 !=1 & hab_67 != 1 & hab_67 != 1 ~ 0) 
+    ) %>%
+  select(-sexo, -vendas_euro, -pessoas, -hnormais, -hextra, -remhextra,-outrasprest,-subsidios, -nacion) %>%
+  rename(year = ano, firm = empresa, base = rembase, college = hab_8910) %>%
   arrange(id, year)
 
 # Analyze first observations
-head(dat)
+head(data)
+
 
 # Create the first differences
-data <- dat %>% 
-  group_by(id,year) %>%
-  mutate(dwage = lag(wage, n=1, default = NA)) 
+data <- data %>% 
+  group_by(id) %>%
+  mutate(dwage = lag(wage, n=1, default = NA),
+         dbonus = lag(bonus, n=1, default = NA),
+         dbase = lag(base, n=1, default = NA),
+         dlbase = lag(lbase, n=1, default = NA),
+         dlbonus = lag(lbonus, n=1, default = NA),
+         dlwage = lag(lwage, n=1, default = NA)) %>%
+  ungroup()
 
-data <- data %>% select(id, year, dwage) 
 
-# write.csv(data, "dwage.csv")
-
-#,
-#         dbonus = , lag(bonus, n=1, default = NA),
- #        dbase = lag(base, n=1, default = NA),
-  #       dlbase = lag(lbase, n=1, default = NA),
-   #      dlbonus = lag(lbonus, n=1, default = NA),
-    #     dlwage = lag(lwage, n=1, default = NA)) %>%
-  #ungroup()
+# Check for other missing variables and finalize new data panel (select)
+# Run regressions
 
 
